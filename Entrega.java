ce4a5c52249a523a525ebfc0064459d5eef84fbe
HugoLoGa <hugoibz05@gmail.com>
2024-05-07 19:55:03 +0200

    Update Entrega.java

--- Entrega.java
+++ Entrega.java
@@ -41,7 +41,7 @@ import java.util.stream.Stream;
  * Podeu fer aquesta entrega en grups de com a màxim 3 persones, i necessitareu com a minim Java 10.
  * Per entregar, posau a continuació els vostres noms i entregau únicament aquest fitxer.
  * - Nom 1: Raúl Sánchez
- * - Nom 2:
+ * - Nom 2:Hugo López García
  * - Nom 3:
  *
  * L'entrega es farà a través d'una tasca a l'Aula Digital que obrirem abans de la data que se us
